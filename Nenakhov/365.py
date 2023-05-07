4459901b86eddc607495689f71f5f3f76062b78c
vanes <73476639+nenakhov22@users.noreply.github.com>
2023-05-08 01:45:57 +0300

    Update 365.py

--- Nenakhov/365.py
+++ Nenakhov/365.py
@@ -9,13 +9,14 @@ class Solution(object):
         my_stack = collections.deque()
         my_stack.append((0,0))
         memory = set((0,0))
+        #пройдемся DFS-ом
         while my_stack:
             arr = my_stack.pop()
             a = arr[0]
             b = arr[1]
             if a+b == targetCapacity:
                 return True
-            
+            #смотрим все возможные дальнейшие операции, которые доступны из условия
             for i in [(jug1Capacity,b), (a,jug2Capacity), (0,b), (a,0), (min(a+b,jug1Capacity), max(b-(jug1Capacity-a),0)), (max(a-(jug2Capacity-b),0),min(a+b,jug2Capacity))]:
                 if i not in memory:
                     memory.add(i)
